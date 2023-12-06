f35c6a27cd4f6094b582e9dac95026a42833a273
Fathi Abdelmalek <abdelmalek.fathi.2001@gmail.com>
2023-12-06 17:02:32 +0100

    design grades and rooms view

--- src/main/java/com/fathi/newrootacademymanager/controllers/others/GradesRoomsViewController.java
+++ src/main/java/com/fathi/newrootacademymanager/controllers/others/GradesRoomsViewController.java
@@ -1,4 +1,46 @@
 package com.fathi.newrootacademymanager.controllers.others;
 
+import com.fathi.newrootacademymanager.models.Grade;
+import com.fathi.newrootacademymanager.models.Room;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class GradesRoomsViewController {
+    @FXML
+    public TextField searchGradeText;
+    @FXML
+    public TextField searchRoomText;
+    @FXML
+    public ListView<Grade> gradesList;
+    @FXML
+    public TextField levelText;
+    @FXML
+    public TextField yearText;
+    @FXML
+    public ListView<Room> roomsList;
+    @FXML
+    public TextField codeText;
+
+    public void initialize() {}
+
+    public void searchGradeAction() {}
+
+    public void searchRoomAction() {}
+
+    public void insertGrade() {}
+
+    public void updateGrade() {}
+
+    public void deleteGrade() {}
+
+    public void clearGrade() {}
+
+    public void insertRoom() {}
+
+    public void updateRoom() {}
+
+    public void deleteRoom() {}
+
+    public void clearRoom() {}
 }
