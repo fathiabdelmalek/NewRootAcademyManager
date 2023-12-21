727da30d901b1f1594917d56a70900631b9e0fe9
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2023-12-21 23:16:48 +0100

    complete the skeleton of the table board view

--- src/main/java/com/fathi/newrootacademymanager/controllers/others/TableBoardViewController.java
+++ src/main/java/com/fathi/newrootacademymanager/controllers/others/TableBoardViewController.java
@@ -1,4 +1,160 @@
 package com.fathi.newrootacademymanager.controllers.others;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class TableBoardViewController {
+//    @FXML
+//    private ComboBox<?> roomChoice;
+    @FXML
+    private Label f5;
+    @FXML
+    private Label fr0;
+    @FXML
+    private Label fr1;
+    @FXML
+    private Label fr2;
+    @FXML
+    private Label fr3;
+    @FXML
+    private Label fr4;
+    @FXML
+    private Label fr6;
+    @FXML
+    private Label fr7;
+    @FXML
+    private Label fr8;
+    @FXML
+    private Label fr9;
+    @FXML
+    private Label mn0;
+    @FXML
+    private Label mn1;
+    @FXML
+    private Label mn2;
+    @FXML
+    private Label mn3;
+    @FXML
+    private Label mn4;
+    @FXML
+    private Label mn5;
+    @FXML
+    private Label mn6;
+    @FXML
+    private Label mn7;
+    @FXML
+    private Label mn8;
+    @FXML
+    private Label mn9;
+    @FXML
+    private Label sn0;
+    @FXML
+    private Label sn1;
+    @FXML
+    private Label sn2;
+    @FXML
+    private Label sn3;
+    @FXML
+    private Label sn4;
+    @FXML
+    private Label sn5;
+    @FXML
+    private Label sn6;
+    @FXML
+    private Label sn7;
+    @FXML
+    private Label sn8;
+    @FXML
+    private Label sn9;
+    @FXML
+    private Label st0;
+    @FXML
+    private Label st1;
+    @FXML
+    private Label st2;
+    @FXML
+    private Label st3;
+    @FXML
+    private Label st4;
+    @FXML
+    private Label st5;
+    @FXML
+    private Label st6;
+    @FXML
+    private Label st7;
+    @FXML
+    private Label st8;
+    @FXML
+    private Label st9;
+    @FXML
+    private Label te0;
+    @FXML
+    private Label te1;
+    @FXML
+    private Label te2;
+    @FXML
+    private Label te3;
+    @FXML
+    private Label te4;
+    @FXML
+    private Label te5;
+    @FXML
+    private Label te6;
+    @FXML
+    private Label te7;
+    @FXML
+    private Label te8;
+    @FXML
+    private Label tt6;
+    @FXML
+    private Label tu0;
+    @FXML
+    private Label tu1;
+    @FXML
+    private Label tu2;
+    @FXML
+    private Label tu3;
+    @FXML
+    private Label tu4;
+    @FXML
+    private Label tu5;
+    @FXML
+    private Label tu7;
+    @FXML
+    private Label tu8;
+    @FXML
+    private Label tu9;
+    @FXML
+    private Label wd0;
+    @FXML
+    private Label wd1;
+    @FXML
+    private Label wd2;
+    @FXML
+    private Label wd3;
+    @FXML
+    private Label wd4;
+    @FXML
+    private Label wd5;
+    @FXML
+    private Label wd6;
+    @FXML
+    private Label wd7;
+    @FXML
+    private Label wd8;
+    @FXML
+    private Label wd9;
+
+    @FXML
+    void initialize() {
+    }
+
+    @FXML
+    void changeRoomAction(ActionEvent event) {
+
+    }
 }
