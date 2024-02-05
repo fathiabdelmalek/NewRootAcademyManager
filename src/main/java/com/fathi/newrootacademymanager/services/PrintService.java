5ef3e4e7caa001b2a838135e31fba4057fd75704
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2024-02-05 22:27:31 +0100

    enhance the code by removing the big sql query from PrintService and add a new parameter to the JasperReport object

--- src/main/java/com/fathi/newrootacademymanager/services/PrintService.java
+++ src/main/java/com/fathi/newrootacademymanager/services/PrintService.java
@@ -22,33 +22,13 @@ public class PrintService {
         try (EntityManager em = dbcm.getFactory().createEntityManager();
             Session session = em.unwrap(Session.class)) {
             session.doWork(connection -> {
-                String sql = "SELECT (\"STUDENTS\".\"FIRST_NAME\" || ' ' || \"STUDENTS\".\"LAST_NAME\") AS \"Student Name\", \n" +
-                        "\t\"ATTENDANCES\".\"TIMES_PRESENT\" AS \"Times Present\",\n" +
-                        "\t\"ATTENDANCES\".\"DUES\" AS \"Dues\",\n" +
-                        "\t\"ATTENDANCES\".\"NOTES\" AS \"Notes\",\n" +
-                        "\t\"LESSONS\".\"LESSON_NAME\" AS \"Lesson Name\",\n" +
-                        "\t\"LESSONS\".\"PRICE\" AS \"Price\",\n" +
-                        "\t(\"TEACHERS\".\"FIRST_NAME\" || ' ' || \"TEACHERS\".\"LAST_NAME\") AS \"Teacher Name\",\n" +
-                        "\t\"LESSONS\".\"PERCENTAGE\" AS \"Percentage\",\n" +
-                        "\t\"LESSONS\".\"TEACHER_DUES\" AS \"Teacher Dues\",\n" +
-                        "FROM \"ATTENDANCES\"\n" +
-                        "\tINNER JOIN \"STUDENTS\" ON \n" +
-                        "\t \"STUDENTS\".\"ID\" = \"ATTENDANCES\".\"STUDENT_ID\"\n" +
-                        "\tINNER JOIN \"LESSONS\" ON \n" +
-                        "\t \"LESSONS\".\"ID\" = \"ATTENDANCES\".\"LESSON_ID\"\n" +
-                        "\tINNER JOIN \"TEACHERS\" ON\n" +
-                        "\t \"TEACHERS\".\"ID\" = \"LESSONS\".\"TEACHER_ID\"\n" +
-                        "WHERE \"LESSONS\".\"ID\" = " + id;
                 try (InputStream inputStream = PrintService.class.getResourceAsStream("/com/fathi/newrootacademymanager/jasper/LessonDetails.jrxml");
                      InputStream logoStream = PrintService.class.getResourceAsStream("/com/fathi/newrootacademymanager/images/logo.png")){
                     if (inputStream != null) {
                         Map<String, Object> parameters = new HashMap<>();
+                        parameters.put("id", id);
                         parameters.put("logoPath", logoStream);
-                        JasperDesign jd = JRXmlLoader.load(inputStream);
-                        JRDesignQuery query = new JRDesignQuery();
-                        query.setText(sql);
-                        jd.setQuery(query);
-                        JasperReport jr = JasperCompileManager.compileReport(jd);
+                        JasperReport jr = JasperCompileManager.compileReport(inputStream);
                         JasperPrint jp = JasperFillManager.fillReport(jr, parameters, connection);
                         JasperPrintManager.printReport(jp, true);
                     } else {
